3223b9c7875ba13eff897c45a6ffda703aaa6a39
AniolCarceller <aniolcarcellerfp@ibf.cat>
2024-11-07 17:52:21 +0100

    Comienzo de la pagina

--- index.php
+++ index.php
@@ -1,2 +1,31 @@
-<?php
-//hola
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wallafood</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <div id="header">
+            <img src="img/logo.png" alt="">
+            <input type="text" name="hola" id="">
+            <a href="">Regístrate o inicia sesíon</a>
+            <a href="">Pedir</a>
+        </div>
+        <div id="menu">
+            <a href="">Inicio</a>
+            <a href="">Carta</a>
+            <a href="">Pedidos</a>
+            <a href="">Reservas</a>
+        </div>
+    </header>
+    <article>
+        <p>a</p>
+    </article>
+    <footer>
+
+    </footer>
+</body>
+</html>
