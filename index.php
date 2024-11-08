66f6bfd3740e8cd15b834c5f9889f00eff8caa8b
AniolCarceller <aniolcarcellerfp@ibf.cat>
2024-11-08 20:09:25 +0100

    Comienzo de implementacion de Bootstrap

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Wallafood</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.css?ver=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
     <header>
@@ -12,7 +13,7 @@
             <img src="img/logo.png" alt="">
             <input type="text" name="hola" id="">
             <a href="">Regístrate o inicia sesíon</a>
-            <a href="">Pedir</a>
+            <a href="">Pedir</a> 
         </div>
         <div id="menu">
             <a href="">Inicio</a>
@@ -21,11 +22,48 @@
             <a href="">Reservas</a>
         </div>
     </header>
-    <article>
-        <p>sakdshadsa</p>
+    <article id="bannerColor">
+        <div id="banner">
+            <div id="bannerLinkCarta">
+                <h1><strong>Prueba</strong> nuestras mejores pizzas</h1>
+                <a href="">Nuestra carta</a>
+            </div>
+            <img src="img/imagenbanner.png" alt="" class="img-fluid">
+        </div>
+    </article>
+    <article class="productosInicio">
+        <h1>Pizzas</h1>
+        <div class="productosAlignInicio">
+            <div class="producto">
+                <img src="img/pizza1.png" alt="">
+                <h3>Melanzani</h3>
+                <p>Berenjena, queso mozzarella, tomate, albahaca fresca, Ajo, aceite de oliva, queso parmesano</p>
+            </div>
+            <div class="producto">
+                <img src="img/pizza2.png" alt="">
+                <h3>Pepperoni</h3>
+                <p>Pepperoni, queso mozzarella, salsa de tomate, orégano</p>
+            </div>
+            <div class="producto">
+                <img src="img/pizza3.png" alt="">
+                <h3>Margherita</h3>
+                <p>Salsa de tomate, queso mozzarella, albahaca fresca, aceite de oliva</p>
+            </div>
+            <div class="producto">
+                <img src="img/pizza4.png" alt="">
+                <h3>Al Capone</h3>
+                <p>Salsa de tomate, queso mozzarella, pepperoni, jamón, salchicha, pimiento, cebolla, aceitunas negras, orégano</p>
+            </div>
+            <div class="producto">
+                <img src="img/pizza5.png" alt="">
+                <h3>The Italian Job</h3>
+                <p>Salsa de tomate, queso mozzarella, jamón serrano, rúcula, tomates cherry, lascas de parmesano, aceite de oliva</p>
+            </div>
+        </div>
     </article>
     <footer>
 
     </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
